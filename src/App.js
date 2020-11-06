f85b433a3d9858b9fb9acc5c58769a82fbf22db1
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-11-06 13:09:14 -0800

    Not finished, passing data up the chain, difficulties with importing info from file to file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [starWarsData,setStarWarsData] = useState({});
-  const [characterData,setCharacterData] = useState([]); 
+  const [characterData,setCharacterData] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+import CharacterInfo from './components/Character_info';
+
+//So how do I map over the individual element to grab name:{name}, like that?
+
 export default function Character({characterData}){
    //want to return Name
     return(
         <div className = "character-container">
-            {characterData}
+           <h1> {characterData}</h1>
              </div>
     );
 
